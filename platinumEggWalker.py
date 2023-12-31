2092e08a190b9c2c46554e5a4fe19f7c7553c840
Tri <trimpham98@gmail.com>
2023-12-31 06:54:49 -0800

    Add code for pokemon platinum

---
+++ platinumEggWalker.py
@@ -0,0 +1,20 @@
+import pyautogui
+import time
+
+# Ensure the failsafe is enabled.
+pyautogui.FAILSAFE = True
+
+# Move the mouse to the specific coordinates and double click
+pyautogui.moveTo(980, 420)
+pyautogui.doubleClick()
+
+while True:
+    # Hold down the 'Up' arrow key
+    pyautogui.keyDown('up')
+    time.sleep(2)  # Keep the key pressed for half a second
+    pyautogui.keyUp('up')  # Release the 'Up' arrow key
+
+    # Hold down the 'Down' arrow key
+    pyautogui.keyDown('down')
+    time.sleep(2)  # Keep the key pressed for half a second
+    pyautogui.keyUp('down')  # Release the 'Down' arrow key
