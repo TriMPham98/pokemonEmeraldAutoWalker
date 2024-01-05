1f270f22064ee7a786215b703b0dba2613862f2b
Tri <trimpham98@gmail.com>
2024-01-05 14:22:46 -0800

    Change comment

--- platinumEggWalker.py
+++ platinumEggWalker.py
@@ -11,10 +11,10 @@ pyautogui.doubleClick()
 while True:
     # Hold down the 'Up' arrow key
     pyautogui.keyDown('up')
-    time.sleep(3)  # Keep the key pressed for half a second
+    time.sleep(3)  # Keep the key pressed for 3 seconds
     pyautogui.keyUp('up')  # Release the 'Up' arrow key
 
     # Hold down the 'Down' arrow key
     pyautogui.keyDown('down')
-    time.sleep(3)  # Keep the key pressed for half a second
+    time.sleep(3)  # Keep the key pressed for 3 seconds
     pyautogui.keyUp('down')  # Release the 'Down' arrow key
